3ec567c687e126051db612b6609e9f2df901879f
Jorge Rodriguez <jorge@headway.io>
2023-06-15 17:11:16 -0400

    update colors

--- app/config/colors.tsx
+++ app/config/colors.tsx
@@ -11,6 +11,7 @@ export default {
   grey3: '#86939e',
   grey4: '#bdc6cf',
   grey5: '#F5F6FA', // light grey
+  white: '#ffffff',
   dkGreyBg: '#232323',
   greyOutline: '#cbd2d9',
   searchBg: '#F5F6FA',
