4d1c5aedaf5b5932af699a4d6f862bdff4e1cefc
Jorge Rodriguez <jorge@headway.io>
2023-06-16 18:08:16 -0400

    login starter component

---
+++ app/login.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Login() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+});
