4e2d99d2bcc652151646ca86058fffe5b4ddea52
Jorge Rodriguez <jorge@headway.io>
2023-06-23 17:04:49 -0400

    Home starter template

--- app/(home)/home.tsx
+++ app/(home)/home.tsx
@@ -13,7 +13,15 @@ export default function Home() {
   console.log('HOME: Session ' + JSON.stringify(session))
   return (
     <View style={styles.container}>
-      <Text>HOME</Text>
+      <View style={styles.topHeader}>
+        <View
+          style={{}}
+        >
+          <Text style={styles.header}>Hello</Text>
+          <Text style={{}}>Welcome to Laza</Text>
+          <Text>{user?.email}</Text>
+        </View>
+      </View>
       <Button title="Sign Out" onPress={signOut} />
     </View>
   );
@@ -22,6 +30,17 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  topHeader: {
+    height: '20%',
+    justifyContent: 'flex-end',
+    paddingBottom: 10,
+    paddingLeft: 10,
+  },
+  header: {
+    fontSize: 28,
+    fontWeight: 'bold',
   },
 });
