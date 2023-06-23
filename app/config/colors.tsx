64eec3ca0b9c9c2a72b2365e846d651edfbe1c51
Jorge Rodriguez <jorge@headway.io>
2023-06-23 17:04:58 -0400

    Add colors

--- app/config/colors.tsx
+++ app/config/colors.tsx
@@ -4,14 +4,16 @@ export default {
   primary2: '#6296f9',
   secondary: '#4BC76D', // green
   secondary2: '#00B233',
-  secondary3: '#34C559',
+  secondary3: '#34C759',
   grey0: '#393e42',
   grey1: '#43484d',
   grey2: '#5e6977',
   grey3: '#8F959E',
   grey4: '#bdc6cf',
   grey5: '#F5F6FA', // light grey
+  grey6: '#E7E8EA',
   white: '#ffffff',
+  black: '#1D1E20',
   dkGreyBg: '#232323',
   greyOutline: '#cbd2d9',
   searchBg: '#F5F6FA',
