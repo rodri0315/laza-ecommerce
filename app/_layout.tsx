97e6cd77362d2281c5dc789ff952f0b1d9f9a797
Jorge Rodriguez <jorge@headway.io>
2023-06-26 09:18:22 -0400

    Add status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider, createTheme } from '@rneui/themed';
 import useCachedResources from './hooks/useCachedResources';
-import Onboarding from "./(auth)/onboarding";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import theme from './config/theme';
-import SignIn from './signin';
 import { AuthProvider } from './contexts/AuthContext';
-import { Slot, Stack } from 'expo-router';
+import { Slot } from 'expo-router';
 
 
 export default function App() {
@@ -20,6 +18,7 @@ export default function App() {
       <SafeAreaProvider>
         <AuthProvider>
           <ThemeProvider theme={theme}>
+            <StatusBar />
             <Slot />
           </ThemeProvider>
         </AuthProvider>
