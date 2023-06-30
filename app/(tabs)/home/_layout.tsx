10b9920ef873264615872046a197787852222fee
Jorge Rodriguez <jorge@headway.io>
2023-06-30 10:47:55 -0400

    hide header

--- app/(tabs)/home/_layout.tsx
+++ app/(tabs)/home/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from 'expo-router'
 
 export default function AppLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="[product]" />
     </Stack>
   )
