465cd9e588cce684df12ab5dea714f7bac7e1e2e
Jorge Rodriguez <jorge@headway.io>
2023-07-03 12:06:38 -0400

    Add DB types, add Review to context

--- app/contexts/ProductContext.tsx
+++ app/contexts/ProductContext.tsx
@@ -3,16 +3,19 @@ import axios from 'axios';
 import { api } from '../services/api';
 import { useAuth } from './AuthContext';
 import { supabaseClient } from '../config/supabase-client';
+import { Database } from '../types/supabase';
 
 // Create a context for products
 interface ProductContextProps {
   products: Product[];
   brands: Brand[];
+  reviews: Review[];
 }
 
 const ProductContext = createContext<ProductContextProps>({
   products: [],
-  brands: []
+  brands: [],
+  reviews: [],
 });
 
 type CurrentProductContextType = {
@@ -42,6 +45,9 @@ export interface Brand {
   logo_url: string;
 }
 
+export type Review = Database['public']['Tables']['reviews']['Row']
+
+
 export function useProducts() {
   return useContext(ProductContext);
 }
@@ -53,6 +59,7 @@ export function useCurrentProduct() {
 export const ProductProvider: React.FC = ({ children }: any) => {
   const [products, setProducts] = useState<Product[]>([]);
   const [brands, setBrands] = useState<Brand[]>([]);
+  const [reviews, setReviews] = useState<Review[]>([]);
   const [currentProduct, setCurrentProduct] = useState<Product | null>(null);
   const { user, session } = useAuth();
   const token = `Bearer ${session?.access_token}`;
@@ -75,7 +82,7 @@ export const ProductProvider: React.FC = ({ children }: any) => {
             'Authorization': token,
           }
         });
-        // console.log("brands", brands.data)
+        if (error) throw error;
         setBrands(brands.data);
         setProducts(response.data);
       } catch (error) {
@@ -86,8 +93,37 @@ export const ProductProvider: React.FC = ({ children }: any) => {
     fetchProducts();
   }, []);
 
+  // fetch reviews by product id
+  useEffect(() => {
+    const fetchReviews = async () => {
+      console.log('currentProduct', currentProduct)
+      try {
+        // const {
+        //   data: { session }, error
+        // } = await supabaseClient.auth.getSession();
+        // const token = `Bearer ${session?.access_token}`;
+        // const token = session?.access_token;
+        // console.log('Review token', token)
+        const reviews = await api.get(`/reviews?product_id=eq.${currentProduct?.id}&select=*`, {
+          headers: {
+            'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRidWRjaHp6bGtvenhid3BjdGZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY3NjcxMzksImV4cCI6MjAwMjM0MzEzOX0.OodwJcw12wGRJzJBzZU3ijUb4wALBGuzahwAsgSdT14',
+            'Authorization': token,
+          }
+        });
+        // if (error) throw error;
+        console.log('reviews', reviews.data)
+        setReviews(reviews.data);
+      } catch (error) {
+        console.error('Error fetching reviews:', error);
+      }
+    };
+
+    if (currentProduct) fetchReviews();
+    // fetchReviews();
+  }, [currentProduct]);
+
   return (
-    <ProductContext.Provider value={{ products, brands }}>
+    <ProductContext.Provider value={{ products, brands, reviews }}>
       <CurrentProductContext.Provider value={{ currentProduct, setCurrentProduct }} >
         {children}
       </CurrentProductContext.Provider>
