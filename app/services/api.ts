edcfbf86e589e55740a4dceb19b65ce03d89c6df
Jorge Rodriguez <jorge@headway.io>
2023-07-05 14:22:07 -0400

    update url string

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://tbudchzzlkozxbwpctfi.supabase.co/rest/v1/',
+  baseURL: 'https://tbudchzzlkozxbwpctfi.supabase.co/rest/v1',
 });
 
 // Products?select=*
