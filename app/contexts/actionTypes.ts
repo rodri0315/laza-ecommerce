edf1b6b9cd95f2e1b4e2656817e9dbc7e66ec923
Jorge Rodriguez <jorge@headway.io>
2023-07-05 10:37:04 -0400

    action types

---
+++ app/contexts/actionTypes.ts
@@ -0,0 +1,13 @@
+export const GET_REVIEWS = "GET_REVIEWS";
+export const ADD_REVIEW = "ADD_REVIEW";
+export const REVIEW_ERROR = "REVIEW_ERROR";
+export const REMOVE_REVIEW = "REMOVE_REVIEW";
+export const TOGGLE_COMPLETED = "TOGGLE_COMPLETED";
+
+// const actions = {
+//   GET_REVIEWS,
+//   ADD_REVIEW,
+//   REVIEW_ERROR,
+//   REMOVE_REVIEW,
+//   TOGGLE_COMPLETED
+// };
