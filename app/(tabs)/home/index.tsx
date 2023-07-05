060582bfb912f78c6c76399ac51f385af19aec74
Jorge Rodriguez <jorge@headway.io>
2023-07-05 15:36:04 -0400

    add index

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -114,7 +114,7 @@ export default function Home() {
       <ScrollView contentContainerStyle={styles.cardList}>
         {
           products.map((product, index) => {
-            return <ProductCard key={index} product={product} index={index} />
+            return <ProductCard key={product.id} product={product} index={index} />
           })
         }
       </ScrollView>
