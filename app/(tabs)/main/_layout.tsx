3976a8fc2fb4bf34aae8e7258fcca599c37ccb64
Jorge Rodriguez <jorge@headway.io>
2023-07-13 14:36:02 -0400

    add icons

--- app/(tabs)/main/_layout.tsx
+++ app/(tabs)/main/_layout.tsx
@@ -1,15 +1,57 @@
 import { Tabs } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import Heart from '../../../assets/Heart.svg'
+import Bag from '../../../assets/CartGray.svg'
+import Wallet from '../../../assets/Wallet.svg'
 
 export default function Layout() {
   return (
     <Tabs screenOptions={{
       headerShown: false,
     }}>
-      <Tabs.Screen name="home" options={{ tabBarLabel: "Home" }} />
-      <Tabs.Screen name="favorites" options={{ tabBarLabel: "Favorites" }} />
-      <Tabs.Screen name="cart" options={{ tabBarLabel: "Cart" }} />
-      <Tabs.Screen name="cards" options={{ tabBarLabel: "Cards" }} />
+      <Tabs.Screen name="home" options={{
+        tabBarLabel: "Home",
+        tabBarShowLabel: true,
+        tabBarIcon: (props) => {
+          return (
+            <>
+            </>
+          );
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: 'bold',
+          marginBottom: 15,
+        }
+      }} />
+      <Tabs.Screen name="favorites" options={{
+        tabBarLabel: "Favorites",
+        tabBarIcon(props) {
+          return (
+            <Heart {...props} />
+          );
+        },
+      }}
+      />
+      <Tabs.Screen name="cart" options={{
+        tabBarLabel: "Cart",
+        tabBarIcon(props) {
+          return (
+            <Bag {...props} />
+          );
+        },
+      }}
+      />
+      <Tabs.Screen name="cards" options={{
+        href: "/(tabs)/main/cart/payment",
+        tabBarLabel: "Cards",
+        tabBarIcon(props) {
+          return (
+            <Wallet {...props} />
+          );
+        },
+      }}
+      />
     </Tabs>
   );
 }
