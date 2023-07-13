909bde21489bf57ae1c08b43f78dd5a7a5fcf86b
Jorge Rodriguez <jorge@headway.io>
2023-07-13 14:36:22 -0400

    favorites screen and favorite button

--- app/(tabs)/main/favorites.tsx
+++ app/(tabs)/main/favorites.tsx
@@ -1,57 +1,197 @@
-// react native screen with title 
-
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { useAuth } from '../../contexts/AuthContext';
-import { Button } from '@rneui/themed';
+import React, { useEffect, useState } from 'react';
+import { View, Text, ScrollView, StyleSheet, SafeAreaView, TouchableOpacity, TextInput } from 'react-native';
+import { Formik, useFormik, useFormikContext } from 'formik';
+import BackButton from '../../components/BackButton';
+import colors from '../../config/colors';
+import CartButton from '../../components/CartButton';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Image } from '@rneui/themed';
+import ProductCard from '../../components/ProductCard';
 import { useProducts } from '../../contexts/ProductContext';
 
-export default function Cart() {
+export default function Favorites() {
 
-  const { products } = useProducts();
+  const [loading, setLoading] = useState(false);
 
-  const { user, session, signOut } = useAuth();
+  const { userFavorites: products } = useProducts();
 
+  useEffect(() => {
+
+  }, [products])
+
+  if (!products) return null;
 
   return (
-    <View style={styles.container}>
-      <View style={styles.topHeader}>
-        <View
-          style={{}}
-        >
-          <Text style={styles.header}>Favorites</Text>
-        </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <BackButton />
+
+        <Text style={styles.title}>Wishlist</Text>
+
+        <CartButton />
       </View>
-      <View>
-        {/* list of products */}
+      <View style={styles.itemSection}>
+        <View style={styles.itemTextContainer}>
+          <Text style={styles.itemTitle}>{products.length} Items</Text>
+          <Text style={styles.itemDescription}>in wishlist</Text>
+        </View>
+        <TouchableOpacity style={styles.sortButton}>
+          <MaterialIcons name="edit" size={18} color="black" />
+          <Text style={styles.sortButtonText}>Edit</Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView contentContainerStyle={styles.cardList}>
         {
           products.map((product, index) => {
-            return (
-              <Text key={index}>{product.name}</Text>
-            )
-          }
-          )
+            return <ProductCard
+              key={product.id}
+              product={product}
+              isFavorite={true}
+              index={index} />
+          })
         }
-      </View>
-      <Button title="Sign Out" onPress={signOut} />
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
+    backgroundColor: colors.white,
+  },
+  reviewStars: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  reviewsInfo: {
+    flexDirection: 'row',
     justifyContent: 'space-between',
+    marginHorizontal: 20,
+    marginTop: 30,
   },
-  topHeader: {
-    height: '20%',
-    justifyContent: 'flex-end',
-    paddingBottom: 10,
-    paddingLeft: 10,
+  reviewsCount: {
+    fontSize: 15,
+    fontWeight: '500',
   },
-  header: {
-    fontSize: 28,
-    fontWeight: 'bold',
+  reviewsRating: {
+    // justifyContent: 'flex-start',
+    // textAlign: 'left',
+    // alignContent: 'flex-start',
+    // alignItems: 'flex-start',
+    // alignSelf: 'flex-start',  
+  },
+  rating: {
+    fontSize: 13,
+    fontWeight: '600',
+  },
+  ratingContainer: {
+    flexDirection: 'row',
+    marginTop: 5,
+  },
+  addReviewButton: {
+    backgroundColor: colors.primary,
+    paddingVertical: 5,
+    padding: 20,
+    alignItems: 'center',
+  },
+  addReviewButtonText: {
+    color: colors.white,
+    fontSize: 13,
+    marginLeft: 5,
+  },
+  reviewListContainer: {
+    marginTop: 10,
+  },
+  reviewInput: {
+    // height: 180,
+  },
+  backButton: {
+
+  },
+  headerTitle: {
+    fontSize: 17,
+    fontWeight: '600',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 15,
+  },
+  formContainer: {
+    marginTop: 20,
+    flexGrow: 1,
+    // backgroundColor: colors.grey1,
+  },
+  input: {
+    padding: 10,
+    backgroundColor: colors.grey5,
+    borderBottomWidth: 0,
+    borderEndColor: 'transparent',
+    borderRadius: 5,
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  inputsContainer: {
+    marginHorizontal: 10,
+  },
+  label: {
+    fontSize: 17,
+    fontWeight: '500',
+    marginBottom: 10,
+    color: colors.black,
+  },
+  sliderContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  cardList: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  itemSection: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginVertical: 10,
+  },
+  itemTextContainer: {},
+  itemTitle: {
+    fontSize: 17,
+    fontWeight: '500',
+  },
+  itemDescription: {
+    fontSize: 15,
+    color: colors.grey3,
+  },
+  sortButton: {
+    flexDirection: 'row',
+    backgroundColor: colors.grey5,
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  sortButtonText: {
+    fontSize: 15,
+    fontWeight: '500',
+    marginLeft: 5,
+  },
+  brandImage: {
+    width: 48,
+    height: 25,
+    margin: 10,
+  },
+  brandImageContainer: {
+    backgroundColor: colors.grey5,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 17,
+    fontWeight: '600',
   },
 });
